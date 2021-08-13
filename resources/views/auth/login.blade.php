9a66cc2880a78def55967d8695990eb5d1da3387
tongtuitui <tongtuitui.bitcoin@gmail.com>
2021-08-13 20:09:16 +0800

    First Commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap-3.1.1/css/bootstrap.min.css') }}">
+</head>
+<body>
+
+<div class="container">
+    <div class="row" style="margin-top:45px">
+        <div class="col-md-4 col-md-offset-4">
+            <h4>Login | Custom Auth</h4><hr>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" class="form-control" name="email" placeholder="Enter email address">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" class="form-control" name="password" placeholder="Enter password">
+                </div>
+                <button type="submit" class="btn btn-block btn-primary">Sign In</button>
+                <br>
+                <a href="">I don't have an account, create new</a>
+            </form>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
